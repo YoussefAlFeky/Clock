c4a30e73e909b42de485b3a40645fe2bdf1430ef
YoussefAlFeky <youssefelfekybuissness@gmail.com>
2024-01-20 16:29:47 +0200

    Clock App

---
+++ main.js
@@ -0,0 +1,28 @@
+
+function showTime(){
+
+    var date = new Date();
+    var hours =  date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+
+    if(hours === 0){
+        hours =12;
+    }
+
+    if(hours > 12){
+        hours =hours-12;
+    }
+
+    hours =( hours < 10 ) ? "0" + hours : hours;
+    minutes =( minutes < 10 ) ? "0" + minutes : minutes;
+    seconds =( seconds < 10 ) ? "0" + seconds : seconds;
+    var Time =  hours + " : " + minutes + " : " + seconds;
+
+  document.getElementById('myContainter').innerHTML = Time;
+
+ 
+  setTimeout(showTime,1000);
+}
+
+showTime();
